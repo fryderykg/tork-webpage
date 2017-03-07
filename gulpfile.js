d85119cef2e412f72e599033755a708bd2cb444d
FryderykG <eszkimos@gmail.com>
2017-03-07 10:42:22 +0100

    add editorconfig file add configure livereload gulp plugin add jQuery to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,98 @@
 // Gulp.js configuration
 const
-    // modules
-    gulp = require('gulp'),
-    newer = require('gulp-newer'),
-    imagemin = require('gulp-imagemin'),
-    htmlclean = require('gulp-htmlclean'),
-    concat = require('gulp-concat'),
-    deporder = require('gulp-deporder'),
-    stripdebug = require('gulp-strip-debug'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    postcss = require('gulp-postcss'),
-    assets = require('postcss-assets'),
-    autoprefixer = require('autoprefixer'),
-    mqpacker = require('css-mqpacker'),
-    cssnano = require('cssnano'),
+  // modules
+  gulp = require('gulp'),
+  newer = require('gulp-newer'),
+  imagemin = require('gulp-imagemin'),
+  htmlclean = require('gulp-htmlclean'),
+  concat = require('gulp-concat'),
+  deporder = require('gulp-deporder'),
+  stripdebug = require('gulp-strip-debug'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-sass'),
+  postcss = require('gulp-postcss'),
+  assets = require('postcss-assets'),
+  autoprefixer = require('autoprefixer'),
+  mqpacker = require('css-mqpacker'),
+  cssnano = require('cssnano'),
+  livereload = require('gulp-livereload'),
 
-    // development mode?
-    devBuild = true,
+  // development mode?
+  devBuild = true,
 
-    // folders
-    folder = {
-        src: 'src/',
-        build: 'build/'
-    }
-    ;
+  // folders
+  folder = {
+    src: 'src/',
+    build: 'build/'
+  }
+  ;
 
 // image processing
 gulp.task('images', function() {
-    var out = folder.build + 'images/';
-    return gulp.src(folder.src + 'images/**/*')
-        .pipe(newer(out))
-        .pipe(imagemin({ optimizationLevel: 5 }))
-        .pipe(gulp.dest(out));
+  var out = folder.build + 'images/';
+  return gulp.src(folder.src + 'images/**/*')
+    .pipe(newer(out))
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest(out));
 });
 
 // HTML processing
 gulp.task('html', ['images'], function() {
-    var
-        out = folder.build + '/',
-        page = gulp.src(folder.src + '**/*.html')
-            .pipe(newer(out));
+  var
+    out = folder.build + '/',
+    page = gulp.src(folder.src + '**/*.html')
+      .pipe(newer(out))
+      .pipe(livereload());
 
-    // minify production code
-    if (!devBuild) {
-        page = page.pipe(htmlclean());
-    }
+  // minify production code
+  if (!devBuild) {
+    page = page.pipe(htmlclean());
+  }
 
-    return page.pipe(gulp.dest(out));
+  return page.pipe(gulp.dest(out));
 });
 
 // JavaScript processing
 gulp.task('js', function() {
 
-    var jsbuild = gulp.src(folder.src + 'js/**/*')
-        .pipe(deporder())
-        .pipe(concat('main.js'));
+  var jsbuild = gulp.src(folder.src + 'js/**/*')
+    .pipe(deporder())
+    .pipe(concat('main.js'))
+    .pipe(livereload());
 
-    if (!devBuild) {
-        jsbuild = jsbuild
-            .pipe(stripdebug())
-            .pipe(uglify());
-    }
+  if (!devBuild) {
+    jsbuild = jsbuild
+      .pipe(stripdebug())
+      .pipe(uglify());
+  }
 
-    return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
+  return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
 
 });
 
 // CSS processing
 gulp.task('css', ['images'], function() {
 
-    var postCssOpts = [
-        assets({ loadPaths: ['images/'] }),
-        autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
-        mqpacker
-    ];
+  var postCssOpts = [
+    assets({ loadPaths: ['images/'] }),
+    autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+    mqpacker
+  ];
 
-    if (!devBuild) {
-        postCssOpts.push(cssnano);
-    }
+  if (!devBuild) {
+    postCssOpts.push(cssnano);
+  }
 
-    return gulp.src(folder.src + 'scss/style.scss')
-        .pipe(sass({
-            outputStyle: 'nested',
-            imagePath: 'images/',
-            precision: 3,
-            errLogToConsole: true
-        }))
-        .pipe(postcss(postCssOpts))
-        .pipe(gulp.dest(folder.build + 'css/'));
+  return gulp.src(folder.src + 'scss/style.scss')
+    .pipe(sass({
+      outputStyle: 'nested',
+      imagePath: 'images/',
+      precision: 3,
+      errLogToConsole: true
+    }))
+    .pipe(postcss(postCssOpts))
+    .pipe(gulp.dest(folder.build + 'css/'))
+    .pipe(livereload());
 
 });
 
@@ -97,18 +101,18 @@ gulp.task('run-all', ['html', 'css', 'js']);
 
 // watch for changes
 gulp.task('watch', function() {
+  livereload.listen();
+  // image changes
+  gulp.watch(folder.src + 'images/**/*', ['images']);
 
-    // image changes
-    gulp.watch(folder.src + 'images/**/*', ['images']);
+  // html changes
+  gulp.watch(folder.src + '**/*', ['html']);
 
-    // html changes
-    gulp.watch(folder.src + '**/*', ['html']);
+  // javascript changes
+  gulp.watch(folder.src + 'js/**/*', ['js']);
 
-    // javascript changes
-    gulp.watch(folder.src + 'js/**/*', ['js']);
-
-    // css changes
-    gulp.watch(folder.src + 'scss/**/*', ['css']);
+  // css changes
+  gulp.watch(folder.src + 'scss/**/*', ['css']);
 
 });
 

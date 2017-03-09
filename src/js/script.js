9fa99d68a61371c4f37710c0a514cfab0dd5aed5
FryderykG <eszkimos@gmail.com>
2017-03-09 15:23:47 +0100

    add smooth scroll and navbar functionality add about section in menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,56 @@
-/**
- * Created by frycek on 07.03.17.
- */
-/* Open the sidenav */
-function openNav() {
-  document.getElementById("mySidenav").style.height = "100%";
-}
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $('a.menu').on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash === "#top") {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1000, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+    else if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 60
+      }, 1000, function(){
 
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("mySidenav").style.height = "0";
-}
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+    closeNav();
+  });
 
-document.getElementById('hamburger').addEventListener('click', openNav);
-document.getElementById('menuCloseBtn').addEventListener('click', closeNav);
+  // NAVBAR FUNCTIONS
+  function openNav() {
+    $('#mySidenav').css('height', '100%');
+  }
 
+  function closeNav() {
+    $('#mySidenav').css('height', '0');
+  }
+  // Event Listeners
+  $('#hamburger').on('click', openNav);
+  $('#menuCloseBtn').on('click', closeNav);
+
+  // change navbar background on scrol
+  $(window).scroll(function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 150;
+
+    if(y_scroll_pos > scroll_pos_test) {
+      $(".navbar").css("background","rgba(0, 0, 0, 0.8)");
+    }
+    else {
+      $(".navbar").css("background","transparent");
+    }
+  });
+
+});
